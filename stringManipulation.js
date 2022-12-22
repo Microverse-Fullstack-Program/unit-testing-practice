d638b10b0817cb520fda5e2ae41323b4b569c6f1
Chere Lemma <chere.lemma@aastu.edu.et>
2022-12-22 18:54:14 +0300

    Update and rename stringlen.js to stringManipulation.js

--- stringManipulation.js
+++ stringManipulation.js
@@ -9,13 +9,6 @@ reverseString = (string) => {
   return string.split("").reverse().join("");
 }
 
-class Calculator {
-  add = (num1, num2) => num1 + num2;
-  subtract = (num1, num2) => num1 - num2;
-  divide = (num1, num2) => num1 / num2;
-  multiply = (num1, num2) => num1 * num2;
-}
-
 capitalizeFirstChar = (string) => {
   return string[0].toUpperCase() + string.slice(1);
 }
