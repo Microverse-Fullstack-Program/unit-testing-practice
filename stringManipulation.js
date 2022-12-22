ab5d2b5ae14ebf80fae076aebb143b067898aa15
Chere Lemma <chere.lemma@aastu.edu.et>
2022-12-22 18:58:50 +0300

    Update stringManipulation.js

--- stringManipulation.js
+++ stringManipulation.js
@@ -1,16 +1,16 @@
-stringLength = (string) => {
+const stringLength = (string) => {
   if (string.length > 0 && string.length <= 10) {
     return string.length;
   }
   throw new Error("string length should be 1-10 char");
 };
 
-reverseString = (string) => {
+const reverseString = (string) => {
   return string.split("").reverse().join("");
 }
 
-capitalizeFirstChar = (string) => {
+const capitalize = (string) => {
   return string[0].toUpperCase() + string.slice(1);
 }
 
-module.exports = { stringLength, reverseString, Calculator, capitalizeFirstChar };
+module.exports = { stringLength, reverseString, capitalize };
